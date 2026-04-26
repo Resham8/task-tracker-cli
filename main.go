04d080f465edf8eccbc4b43e705e8e64dbdcc796
Resham <reshamupadhyay48@gmail.com>
2026-04-26 18:46:03 +0530

    added delete func

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
+	"strconv"	
 	"time"
 )
 
@@ -31,12 +31,18 @@ func main() {
 
 	loadTasks()
 
-	if len(args) < 3 {
-		fmt.Println("Usage: go run main.go add \"task description\"")
+	if len(args) < 2 {
+		fmt.Println("Usage:")
+		fmt.Println("  add \"task\"")
+		fmt.Println("  list")
+		fmt.Println("  update <id> \"task\"")
+		fmt.Println("  status <id> <todo|in-progress|done>")
 		return
 	}
 
 	switch args[1] {
+	case "list":
+		listTasks()
 	case "add":
 		description := args[2]
 		addTask(description)
@@ -50,6 +56,26 @@ func main() {
 
 		description := args[3]
 		updateTask(uint(id), description)
+
+	case "status":
+		id, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("Invalid Id")
+			return
+		}
+
+		status := args[3]
+		updateStatus(uint(id), Status(status))
+
+	case "delete":
+		id, err := strconv.Atoi(args[2])
+		if err != nil {
+			fmt.Println("Invalid Id")
+			return
+		}
+		
+		deleteTask(uint(id))
+
 	}
 
 }
@@ -76,7 +102,7 @@ func saveTasks() error {
 
 func addTask(description string) {
 	task := Task{
-		Id:          uint(len(tasks) + 1),
+		Id:          getNextID(),
 		Description: description,
 		Status:      Todo,
 		CreatedAt:   time.Now().Format("02 Jan 2006 03:04 PM"),
@@ -100,4 +126,54 @@ func updateTask(id uint, newDescription string) {
 	}
 
 	fmt.Println("Task not found")
+}
+
+func updateStatus(id uint, newStatus Status) {
+	for i := range tasks {
+		if tasks[i].Id == id {
+			tasks[i].Status = newStatus
+			tasks[i].UpdatedAt = time.Now().Format("02 Jan 2006 03:04 PM")
+
+			saveTasks()
+			fmt.Println("Task marked as ", newStatus)
+			return
+		}
+	}
+}
+
+func listTasks() {
+	if len(tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return
+	}
+
+	for _, t := range tasks {
+		fmt.Printf("[%d] %s\n", t.Id, t.Description)
+		fmt.Printf("   Status: %s\n", t.Status)
+		fmt.Printf("   Created: %s\n", t.CreatedAt)
+		fmt.Printf("   Updated: %s\n\n", t.UpdatedAt)
+	}
+}
+
+func deleteTask(id uint) {
+	for i := range tasks {
+		if tasks[i].Id == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			saveTasks()
+			fmt.Println("Task deleted")
+			return
+		}
+	}
+
+	fmt.Println("Task not found")
+}
+
+func getNextID() uint {	
+	var max uint
+	for _, t := range tasks {
+		if t.Id > max {
+			max = t.Id
+		}
+	}
+	return max + 1
 }
